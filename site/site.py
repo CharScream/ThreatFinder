fe380a0d5651fd2d5d07e8349654de4e15776410
Alec <alecyang@uvic.ca>
2019-07-06 22:42:32 -0700

    moved site files around

--- site/site.py
+++ site/site.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'this is a Flask'
+    if request.method == 'POST':
+        team = request.form['team']
+        return render_template('team.html', team=team)
+    return render_template('Form.html')
+
+
+@app.route('/send', methods=['GET', 'POST'])
+def send():
+    if request.method == 'POST':
+        team = request.form['team']
+        return render_template('team.html', team=team)
+    return render_template('Form.html')
 
 
 app.run('127.0.0.1', debug=True)
