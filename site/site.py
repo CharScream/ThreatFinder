f7a4aa3c3fe3ea513de4e57c1c1b96daee00b67b
Alec <alecyang@uvic.ca>
2019-07-10 22:09:45 -0700

    Able to get input form user and generate new page, started on parser

--- site/site.py
+++ site/site.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         team = request.form['team']
-        return render_template('team.html', team=team)
-    return render_template('Form.html')
+        return render_template('result.html', team=team)
+    return render_template('index.html')
 
 
 @app.route('/send', methods=['GET', 'POST'])
 def send():
     if request.method == 'POST':
         team = request.form['team']
-        return render_template('team.html', team=team)
+        return render_template('result.html', team=team)
     return render_template('Form.html')
 
 
