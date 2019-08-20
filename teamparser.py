f56ca5f6f8cbb655fdd1a5d0e6c7afa8b690163c
Alec <alecyang@uvic.ca>
2019-08-20 01:35:04 -0700

    Got a working pattern, need to work on how to deal with the object from match

--- teamparser.py
+++ teamparser.py
@@ -1,15 +1,17 @@
 import re
+
+
 def parse(team):
-    team_pat = re.compile(r"\b(.*)\s@\s(.*)\r\n.*:\s(.*)\r\n.*:\s(.*)\r\n(.*)\s[Nn].*\r\n")
+    team_pat = re.compile(
+        r"\b(.*)\s@\s(.*)\r\n.*:\s(.*)\r\n.*:\s(.*)\r\n(.*)\s[Nn].*\r\n(?:IVs:\s(.*)\r\n)?-\s(.*)\r\n-\s(.*)\r\n-\s(.*)\r\n-\s(.*)\r\n")
     a = team_pat.findall(team)
     for i in a:
         print(i)
 
-def main():
 
+def main():
     pass
 
 
 if __name__ == "main":
     main()
-
