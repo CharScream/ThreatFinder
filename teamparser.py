ad5b2c283a03900ce7d6beb14adb0ac4f0374edc
Alec <alecyang@uvic.ca>
2019-08-22 20:15:22 -0700

    Got rid of the rightside space on the last move. need to add check for shiny

--- teamparser.py
+++ teamparser.py
@@ -3,7 +3,7 @@ import re
 
 def parse(team):
     team_pat = re.compile(
-        r"\b(.*)\s@\s(.*)\s\r\n.*:\s(.*)\s\r\n.*:\s(.*)\s\r\n(.*)\s[Nn].*\r\n(?:[Ss].*\s.*\r\n)?(?:IVs:\s.*\s\r\n)?-\s(.*)\s\r\n-\s(.*)\s\r\n-\s(.*)\s\r\n-\s(.*)\r\n")
+        r"(.*)\s@\s(.*)\s?\r\n.*:\s(.*)\s?\r\n.*:\s(.*)\s?\r\n(.*)\s[Nn].*\s?\r\n(?:IVs:\s.*\s?\r\n)?-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\r\n?")
     mon_list = team_pat.findall(team)
     for mon in mon_list:
         print(mon)
