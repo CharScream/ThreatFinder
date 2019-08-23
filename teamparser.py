52ad564002dc3aef72d809d588f28d039dabc9c4
Alec <alecyang@uvic.ca>
2019-08-22 21:12:20 -0700

    parser works, need to clean data some more, need lower case and maybe something else

--- teamparser.py
+++ teamparser.py
@@ -1,13 +1,23 @@
 import re
 
-
 def parse(team):
+    complete_mon_list = []
     team_pat = re.compile(
-        r"(.*)\s@\s(.*)\s?\r\n.*:\s(.*)\s?\r\n.*:\s(.*)\s?\r\n(.*)\s[Nn].*\s?\r\n(?:IVs:\s.*\s?\r\n)?-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\r\n?")
+        r"(.*)\s@\s(.*)\s?\r\n.*:\s(.*)\s?\r\n(?:Shiny:.*\s?\r\n)?.*:\s(.*)\s?\r\n(.*)\s[Nn].*\s?\r\n(?:IVs:\s.*\s?\r\n)?-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\s?\r\n-\s(.*)\r\n?")
+    
     mon_list = team_pat.findall(team)
+
+    print(type(mon_list))
+
     for mon in mon_list:
+        temp_li = list(mon)
+        temp_li = [field.rstrip() for field in temp_li]
+        complete_mon_list.append(tuple(temp_li))
+
+    for mon in complete_mon_list:
         print(mon)
-    return mon_list
+
+    return complete_mon_list
 
 
 def main():
