779c22b7f4895be3326c2f023821defe14818b22
Alec <alecyang@uvic.ca>
2019-09-02 10:04:05 -0700

    reads in the csv file into a hash map of mon name and tuple of counters

--- app/csv_to_dict.py
+++ app/csv_to_dict.py
@@ -1,20 +1,29 @@
-import sys 
+import csv
 
-string = "Apple,Red,Green,Round,Fruit,,"
+# string = "Apple,Red,Green,Round,Fruit,,"
 
-li = string.split(",")
-for i in li:
-  print(i)
-print(li)
+# li = string.split(",")
+# for i in li:
+#   print(i)
+# print(li)
 
-li = list(filter(None, li))
+# li = list(filter(None, li))
 
-print(li)
+# print(li)
+
 def build_dict(file_name:str) -> dict:
-	file = open(file_name)
+	di = {}
+	with open(file_name, newline="") as csvfile:
+		reader = csv.reader(csvfile, delimiter= ",", quotechar="|")
+		for row in reader:
+			row_li = list(filter(None,row))
+			di[row_li[0]] = tuple(row_li[1:])
+	
+	return di
 
 def main():
 	print("hey")
+	build_dict("monChecklist.csv")
 
 if __name__ == "__main__":
 	main()
