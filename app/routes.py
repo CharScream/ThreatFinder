3ab5273e1b137ce5b63f43e3a0ed07aec83acd2e
Alec <alecyang@uvic.ca>
2019-12-13 20:56:06 -0800

    added a sample team and fixed some bugs caused by incorrect names in checklist csv.

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,62 @@ INDEX_PAGE = 'index.html'
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method != 'POST':
-        return render_template(INDEX_PAGE)
+        return render_template(INDEX_PAGE, team="""
+Try this sample input below!
+
+Groudon @ Red Orb
+Ability: Drought
+EVs: 144 HP / 156 Atk / 56 SpD / 152 Spe
+Adamant Nature
+- Stealth Rock
+- Swords Dance
+- Precipice Blades
+- Rock Tomb
+
+Ho-Oh @ Leftovers
+Ability: Regenerator
+EVs: 252 HP / 204 Def / 52 SpD
+Impish Nature
+- Sacred Fire
+- Recover
+- Toxic
+- Defog
+
+Kyogre @ Blue Orb
+Ability: Drizzle
+EVs: 4 Def / 252 SpA / 252 Spe
+Modest Nature
+- Water Spout
+- Calm Mind
+- Origin Pulse
+- Ice Beam
+
+Rayquaza @ Life Orb
+Ability: Air Lock
+EVs: 252 Atk / 4 Def / 252 Spe
+Adamant Nature
+- Dragon Ascent
+- Dragon Dance
+- Extreme Speed
+- Earthquake
+
+Xerneas @ Choice Scarf
+Ability: Fairy Aura
+EVs: 4 Def / 252 SpA / 252 Spe
+Modest Nature
+- Moonblast
+- Focus Blast
+- Aromatherapy
+- Defog
+
+Arceus-Rock @ Stone Plate
+Ability: Multitype
+EVs: 252 HP / 16 Def / 144 SpA / 96 Spe
+Timid Nature
+- Judgment
+- Will-O-Wisp
+- Recover
+- Stealth Rock""")
 
     team = request.form['team']
     return render_template(RESULT_PAGE, team=team)
@@ -30,4 +85,4 @@ def result():
     return render_template(RESULT_PAGE, team=input_team, threats=threats)
 
 
-# app.run('127.0.0.1', debug=True)
+app.run('127.0.0.1', debug=True)
